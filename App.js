51c1c1c8b81a0a29fa2828746547873f6065467d
Alfredcelorio <alfredcelorio@gmail.com>
2023-10-04 17:54:28 -0400

    New commit

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useContext } from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import firebase from 'firebase'; // Using the Firebase SDK provided by Expo
+import { AuthContext } from './path_to_your_context'; // Assuming you have a context for authentication
 
-export default function App() {
+const AdminScreen = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const navigation = useNavigation();
+  const { user, signOut } = useContext(AuthContext); // Assuming you have a context handling authentication
+
+  useEffect(() => {
+    // Logic similar to initState
+    // ...
+  }, []);
+
+  const _buildDrawer = (role) => {
+    // This will be a mock of the drawer, you can further implement using `react-navigation` Drawer
+    return (
+      <View style={styles.drawerContainer}>
+        {role !== 'Staff' && (
+          <TouchableOpacity onPress={() => setCurrentPage(4)}>
+            <Text style={styles.drawerItemText}>Landing Page</Text>
+          </TouchableOpacity>
+        )}
+        {/* Add other items similarly */}
+      </View>
+    );
+  };
+
+  const _buildPopupMenuButton = (role) => {
+    // You can use a library like 'react-native-popup-menu' for a similar effect
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* App Bar */}
+      <View style={styles.appBar}>
+        <TouchableOpacity onPress={() => { /* Logic to open drawer */ }}>
+          <Image source={require('./path_to_your_icon.png')} />
+        </TouchableOpacity>
+        <Image source={require('./path_to_your_logo.png')} style={styles.logo} />
+        {_buildPopupMenuButton('Staff')} {/* example role passed */}
+      </View>
+
+      {/* Page Content */}
+      <View style={styles.content}>
+        {/* Display the page based on currentPage state */}
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
+  },
+  appBar: {
+    height: 60, // or whatever height you want
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 16,
+  },
+  logo: {
+    width: 100,
+    resizeMode: 'contain',
+  },
+  content: {
+    flex: 1,
+  },
+  drawerContainer: {
+    flex: 1,
+    backgroundColor: '#1F1F1F',
+  },
+  drawerItemText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 8,
   },
 });
+
+export default AdminScreen;
