2a072383c3e38bab3e508abb6f8ebc8dbf5bc462
Alfredcelorio <alfredcelorio@gmail.com>
2023-10-05 02:22:14 -0400

    Screens

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// Updated paths to point to the 'screens' folder
+//OAuth client created for ios, android and web
+//web 286050939937-mr6d95mcbsop05lii7qk3jve2ldjs4fg.apps.googleusercontent.com
+//ios 286050939937-8euod7ujq8uq1ktqb4pmdjd5hbhsek41.apps.googleusercontent.com
+//android 286050939937-d79lhlsek38tr84id25e4v2rqplpvn9o.apps.googleusercontent.com
+
+import WelcomeScreen from './screens/WelcomeScreen';
+import FutureScreen from './screens/FutureScreen';
+import LoginScreen from './screens/LoginScr';  // Import the new screen
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your apps!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome" headerMode="none">
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Future" component={FutureScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} /> 
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/LoginScr.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+
+export default function App({ navigation }) {
+  const handleGoogleLogin = () => {
+    // Logic to handle Google login goes here
+    console.log("Google login pressed");
+  };
+
+  const handleStartOver = () => {
+    navigation.navigate('Welcome'); 
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcomeText}>Lets Rock and Roll</Text>
+      <TouchableOpacity style={styles.googleLoginButton} onPress={handleGoogleLogin}>
+        <Text style={styles.buttonText}>Log in with Google</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.startOverButton} onPress={handleStartOver}>
+        <Text style={styles.buttonText}>Start Over</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 50,
+  },
+  welcomeText: {
+    fontSize: 52,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  googleLoginButton: {
+    position: 'absolute',
+    bottom: 50,    // Push it closer to the bottom
+    left: '5%',   // Center it horizontally with 90% width
+    width: '90%', // Set width to 90% of the screen
+    padding: 15,
+    backgroundColor: '#4285F4',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  startOverButton: {
+    position: 'absolute', // Position it absolutely
+    bottom: 120,  // Position it just above the googleLoginButton
+    left: '5%',  // Center it horizontally with 90% width
+    width: '90%',
+    padding: 15,
+    backgroundColor: '#FF5733',
+    borderRadius: 10,
+    alignItems: "center",
+  },
+
+});
