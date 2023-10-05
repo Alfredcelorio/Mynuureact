000683aaed8578f35252739549537470618e6a8f
Alfredcelorio <alfredcelorio@gmail.com>
2023-10-05 03:27:00 -0400

    Typo on Start over button

--- APP/screens/LoginScr.js
+++ APP/screens/LoginScr.js
@@ -8,7 +8,7 @@ export default function LoginScr({ navigation }) {
   };
 
   const handleStartOver = () => {
-    navigation.navigate('Welcome'); 
+    navigation.navigate('WelcomeScreen'); 
   };
 
   return (
