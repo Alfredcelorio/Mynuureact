52a989e80455542122bf0bafce717abde06a8180
Alfredcelorio <alfredcelorio@gmail.com>
2023-10-17 20:28:09 -0400

    New menu screen and product detail screen

--- APP/screens/Mainmenu.js
+++ APP/screens/Mainmenu.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, SafeAreaView, TextInput, Image, ScrollView, Dimensions, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, TextInput, Image, ScrollView, Dimensions, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useNavigation } from '@react-navigation/native';
 
-
-
 const { width } = Dimensions.get('window');
-
-
 
 const Product = ({ title, description, price, navigation }) => (
   <View style={styles.productContainer}>
     <View style={styles.imageWrapper}>
-    <TouchableOpacity onPress={() => navigation.navigate('ProductDetail')}>
-    <Image
-      source={{ uri: 'https://images.prismic.io/claseazul/54230971-073d-4a3f-a19e-5b33cf618281_Reposado-NBI.png?auto=compress,format&rect=12,0,507,1559&w=532&h=1636' }}
-      style={styles.productImage}
-      resizeMode="contain"
-    />
-     </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate('ProductDetail')}>
+        <Image
+          source={{ uri: 'https://images.prismic.io/claseazul/54230971-073d-4a3f-a19e-5b33cf618281_Reposado-NBI.png?auto=compress,format&rect=12,0,507,1559&w=532&h=1636' }}
+          style={styles.productImage}
+          resizeMode="contain"
+        />
+      </TouchableOpacity>
     </View>
     <Text style={styles.productTitle}>{title}</Text>
     <Text style={styles.productDescription}>{description}</Text>
@@ -26,16 +22,14 @@ const Product = ({ title, description, price, navigation }) => (
   </View>
 );
 
-const MyApp = () => {
-    const navigation = useNavigation(); 
+const Mainmenu = () => {
+  const navigation = useNavigation();
   const [searchText, setSearchText] = React.useState('');
 
   const products = [
-    { title: "Clase azul", description: "A delightful mix of flavors.", price: "$3000",  },
+    { title: "Clase azul", description: "A delightful mix of flavors.", price: "$3000" },
     // ... (You can add more products here if needed)
   ];
-
- 
 
   const categories = [
     { name: 'Featured Items', products: products },
@@ -48,7 +42,7 @@ const MyApp = () => {
     if (category.name.toLowerCase().includes(searchText.toLowerCase())) {
       return true;
     }
-    return category.products.some(product => 
+    return category.products.some(product =>
       product.title.toLowerCase().includes(searchText.toLowerCase()) ||
       product.description.toLowerCase().includes(searchText.toLowerCase())
     );
@@ -56,22 +50,23 @@ const MyApp = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-       <LinearGradient colors={['#000', 'transparent']} style={styles.gradient}>
-    <View style={styles.logoContainer}>
-      <Image source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/fullaccezz-2756a.appspot.com/o/products%2F41c53523-ce06-4502-aad3-29b8545ee02d?alt=media&token=4932c29c-1f4c-413e-894d-1ca4035dd141' }} style={styles.logo} />
-    </View>
-    </LinearGradient>
-    <View style={styles.topBar}>
-      <Text style={styles.headerText}> Hello Alvaro,</Text>
-
-    </View>
- 
+      <LinearGradient colors={['#000', 'white']} style={styles.gradient}>
+        <View style={styles.logoContainer}>
+          <Image source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/fullaccezz-2756a.appspot.com/o/products%2F41c53523-ce06-4502-aad3-29b8545ee02d?alt=media&token=4932c29c-1f4c-413e-894d-1ca4035dd141' }} style={styles.logo} />
+        </View>
+      </LinearGradient>
+      <View style={styles.topBar}>
+        <Text style={styles.headerText}> Hello,</Text>
+      </View>
       <View style={styles.searchBarContainer}>
         <Text style={styles.headerText}> This is your drink menu</Text>
-        <TextInput 
-          style={styles.searchBar} 
-          placeholder="Search..." 
-          placeholderTextColor="#aaa" 
+        <TouchableOpacity style={styles.buttonContainer} onPress={() => navigation.navigate('CustomDropdown')}>
+  <Text style={styles.buttonText}>Change menu</Text>
+</TouchableOpacity>
+        <TextInput
+          style={styles.searchBar}
+          placeholder="Search..."
+          placeholderTextColor="#aaa"
           value={searchText}
           onChangeText={setSearchText}
         />
@@ -82,45 +77,46 @@ const MyApp = () => {
             <Text style={styles.categoryText}>{category.name}</Text>
             <View style={styles.content}>
               {[...Array(5)].map((_, i) => (
-                <Product key={i} title="Clase azul" description="A delightful mix of flavors." price="$3000"  navigation={navigation} />
+                <Product key={i} title="Clase azul" description="A delightful mix of flavors." price="$3000" navigation={navigation} />
               ))}
             </View>
           </View>
-          
         ))}
       </ScrollView>
+      {/* Button component */}
+      
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-    productContainer: {
-        marginBottom: 20,
-        alignItems: 'center',
-        width: width * 0.5 - 16,
-      },
-      productImage: {
-        width: 263,
-        height: 174,
-      },
-      productTitle: {
-        fontSize: 14,
-        fontWeight: '500',
-        lineHeight: 17,
-        color: '#FFF',
-      },
-      productDescription: {
-        fontSize: 8,
-        fontWeight: '500',
-        lineHeight: 8,
-        color: '#FFF',
-      },
-      productPrice: {
-        fontSize: 24,
-        fontWeight: '600',
-        lineHeight: 24,
-        color: '#FFF',
-      },
+  productContainer: {
+    marginBottom: 20,
+    alignItems: 'center',
+    width: width * 0.5 - 16,
+  },
+  productImage: {
+    width: 263,
+    height: 174,
+  },
+  productTitle: {
+    fontSize: 14,
+    fontWeight: '500',
+    lineHeight: 17,
+    color: '#FFF',
+  },
+  productDescription: {
+    fontSize: 8,
+    fontWeight: '500',
+    lineHeight: 8,
+    color: '#FFF',
+  },
+  productPrice: {
+    fontSize: 24,
+    fontWeight: '600',
+    lineHeight: 24,
+    color: '#FFF',
+  },
   container: {
     flex: 1,
     backgroundColor: '#000',
@@ -133,17 +129,13 @@ const styles = StyleSheet.create({
   },
   logoContainer: {
     alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 5
+    marginTop: 2,
+    marginBottom: 2
   },
   logo: {
-    width: 400,
-    height: 200,
+    width: 180,
+    height: 50,
     resizeMode: 'contain',
-  },
-  welcomeContainer: {
-    padding: 10,
-    alignItems: 'flex-start',
   },
   headerText: {
     fontFamily: 'Metropolis-SemiBold',
@@ -153,8 +145,8 @@ const styles = StyleSheet.create({
     lineHeight: 28,
     letterSpacing: -0.165,
     textAlign: 'left',
-    marginBottom: 1, 
-},
+    marginBottom: 1,
+  },
   searchBarContainer: {
     padding: 10
   },
@@ -184,12 +176,19 @@ const styles = StyleSheet.create({
   imageWrapper: {
     width: 263,
     height: 174,
-    borderRadius: 100,
     overflow: 'hidden',
-},
-
-  
+  },
+  buttonContainer: {
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 10,
+    margin: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 15,
+  },
 });
 
-
-export default MyApp;
+export default Mainmenu;
--- APP/screens/ProductDetailScreen.js
+++ APP/screens/ProductDetailScreen.js
@@ -1,61 +1,141 @@
-import React from 'react';
-import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Image, StyleSheet, Text, TouchableOpacity, ScrollView, useWindowDimensions } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import * as Animatable from 'react-native-animatable';
+
+Animatable.initializeRegistryWithDefinitions({
+  typingFade: {
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+  },
+});
+
+const TypingText = Animatable.createAnimatableComponent(Text);
+const TypingView = Animatable.createAnimatableComponent(View);
 
 const ProductScreen = () => {
   const navigation = useNavigation();
+  const windowWidth = useWindowDimensions().width;
+  const [animationDelay, setAnimationDelay] = useState(0);
+
+  useEffect(() => {
+    // Set a delay before the animation starts (for a typing effect)
+    const delay = 1000; // You can adjust this delay according to your preference
+    setAnimationDelay(delay);
+  }, []);
+
+  const isLandscape = windowWidth > 500;
+
+  const productData = {
+    name: 'Clase azul',
+    description: 'Clase Azul Tequila Reposado is a symbol of Mexican tradition and culture. Made with slow-cooked 100% Blue Weber Agave, our ultra-premium reposado tequila is unique and incomparable.',
+    price: '$3000',
+    // Additional information based on the type of liquor
+    additionalInfo: {
+      ABV: '40%', // Alcohol by volume
+      Body: 'Full-bodied',
+      Brand: 'Clase Azul',
+      CountryState: 'Mexico',
+      Region: 'Jalisco',
+      Sku: '123456789',
+      Taste: 'Smooth and rich',
+      Type: 'Tequila',
+      Varietal: 'Blue Agave',
+    },
+  };
 
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <TouchableOpacity style={styles.goBackButton} onPress={() => navigation.goBack()}>
         <Text style={styles.goBackText}>← Go Back</Text>
       </TouchableOpacity>
 
       <Image 
-        source={{ uri: 'https://firebasestorage.googleapis.com/v0/b/fullaccezz-2756a.appspot.com/o/restaurants%2FLaveinte-logo.jpg?alt=media&token=e0dfe8ef-4177-4885-b30e-a3d157a684c4' }}
+        source={{ uri: 'https://images.prismic.io/claseazul/54230971-073d-4a3f-a19e-5b33cf618281_Reposado-NBI.png?auto=compress,format&rect=12,0,507,1559&w=532&h=1636' }}
         style={styles.image}
         resizeMode="contain"
       />
 
-      <Text style={styles.title}>Clase azul</Text>
-      <Text style={styles.description}>A premium quality tequila crafted with precision and care.</Text>
+      <TypingText
+        animation="typingFade"
+        duration={800}
+        delay={animationDelay}
+        style={styles.title}
+      >
+        {productData.name}
+      </TypingText>
+
+      <TypingText
+        animation="typingFade"
+        duration={1000}
+        delay={animationDelay + 600} // Add delay for a typing effect
+        style={styles.description}
+      >
+        {productData.description}
+      </TypingText>
+
       <View style={styles.priceContainer}>
-        <Text style={styles.price}>$3000</Text>
+        <TypingText
+          animation="typingFade"
+          duration={2000}
+          delay={animationDelay + 900} // Add delay for a typing effect
+          style={styles.price}
+        >
+          {productData.price}
+        </TypingText>
       </View>
-    </View>
+
+      <TypingView // Apply fade-in effect to the entire additional info container
+        animation="typingFade"
+        duration={1000} // Adjust the duration according to your preference
+        delay={animationDelay + 1200} // Add delay for a typing effect
+        style={styles.additionalInfoContainer} // Apply styles to the TypingView component
+      >
+        <ScrollView>
+          {Object.entries(productData.additionalInfo).map(([key, value]) => (
+            <View style={styles.additionalInfoItem} key={key}>
+              <Text style={styles.additionalInfoLabel}>{key}</Text>
+              <Text style={styles.additionalInfoValue}>{value}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </TypingView>
+    </ScrollView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
+    backgroundColor: 'black', // Set background color to black
     paddingHorizontal: 20,
   },
   goBackButton: {
     position: 'absolute',
-    top: 40,
-    left: 20,
+    top: 50,
+    left: 40,
   },
   goBackText: {
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: '500',
-    color: 'white',
+    color: 'white', // Set text color to white
   },
   image: {
-    width: 300, // You can adjust this as per your requirement
-    height: 150, // You can adjust this as per your requirement
+    width: 300,
+    height: 500,
+    marginTop: 70,
   },
   title: {
-    fontFamily: 'Metropolis-Bold',
+    fontFamily: 'Metropolis-Medium',
     fontSize: 24,
     fontWeight: '600',
     lineHeight: 29,
     letterSpacing: -0.25437501072883606,
     textAlign: 'left',
     marginTop: 20,
+    color: 'white', // Set text color to white
   },
   description: {
     fontFamily: 'Metropolis-Bold',
@@ -64,21 +144,80 @@ const styles = StyleSheet.create({
     lineHeight: 20,
     textAlign: 'left',
     marginTop: 10,
+    color: 'white', // Set text color to white
   },
   priceContainer: {
     width: 115,
     height: 28,
     borderWidth: 1,
-    borderColor: '#000',
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start', // Align text to the left
+    alignItems: 'flex-start', // Align text to the left
     marginTop: 20,
+},
+price: {
+    fontFamily: 'Metropolis-SemiBold',
+    fontSize: 28,
+    fontWeight: '600',
+    lineHeight: 28,
+    letterSpacing: 0,
+    textAlign: 'left',
+    color: 'white', // Set text color to white
+},
+  additionalInfoContainer: {
+    marginTop: 20,
+    width: '100%',
+    paddingHorizontal: 20,
   },
-  price: {
+  additionalInfoItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  additionalInfoLabel: {
     fontFamily: 'Metropolis-Bold',
     fontSize: 16,
     fontWeight: '500',
-  }
+    color: 'white', // Set text color to white
+  },
+  additionalInfoValue: {
+    fontFamily: 'Metropolis-Regular',
+    fontSize: 16,
+    fontWeight: '400',
+    color: 'white', // Set text color to white
+  },
+  reservationButton: {
+    position: 'absolute',
+    bottom: '5%',
+    width: '90%',
+    padding: 15,
+    backgroundColor: 'white', // Set background color to white
+    borderColor: 'black', // Set border color to black
+    borderWidth: 1,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  reservationButtonText: {
+    fontFamily: 'Metropolis-ExtraBold',
+    color: 'black', // Set text color to black
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  content: {
+    flexDirection: 'column', // Vertical layout by default
+    alignItems: 'center', // Align items vertically in the center
+  },
+  landscapeContent: {
+    flexDirection: 'row', // Horizontal layout for landscape mode
+    alignItems: 'flex-start', // Align items to the left
+  },
+  imageContainer: {
+    flex: 1, // Take 50% of the available width in landscape mode
+    justifyContent: 'flex-start', // Align child items to the start (left) of the container
+  },
+  textContainer: {
+    flex: 1, // Take 50% of the available width in landscape mode
+    paddingHorizontal: 20,
+  },
 });
 
 export default ProductScreen;
