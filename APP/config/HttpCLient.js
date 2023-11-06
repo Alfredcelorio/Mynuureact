4ec976867372048fc50d54c5607298c52e09dea3
DrNFox <biltd.88@gmail.com>
2023-11-06 19:03:22 -0400

    change

--- APP/config/HttpCLient.js
+++ APP/config/HttpCLient.js
@@ -7,7 +7,7 @@ const production =
 const stage = "http://10.0.2.2:5001/fullaccezz-2756a/us-central1/app/";
 
 const client = axios.create({
-  baseURL: stage,
+  baseURL: production,
   timeout: 30000,
   headers: { "Content-Type": "application/json" },
 });
